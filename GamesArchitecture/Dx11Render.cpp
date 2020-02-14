b827b7a54857cf7e947f3a5f5c9dca5b4ad0deef
Ben Marshall <ben321mars@gmail.com>
2020-02-14 14:31:28 +0000

    Create Dx11Render class

---
+++ GamesArchitecture/Dx11Render.cpp
@@ -0,0 +1,224 @@
+#include "DX11Render.h"
+#include "Win32Window.h"
+#include <vector>
+
+Dx11Render * Dx11Render::mInstance = nullptr;
+
+Dx11Render::Dx11Render() : mDriverType(D3D_DRIVER_TYPE_HARDWARE), mFeatureLevel(D3D_FEATURE_LEVEL_10_0), mDevice(nullptr), mDeviceContext(nullptr),
+mSwapChain(nullptr), mSwapChain1(nullptr), mRenderTargetView(nullptr), mDepthView(nullptr),
+mRasterizerState(nullptr)
+{
+}
+
+bool Dx11Render::loadRender()
+{
+	const auto window = Win32Window::instance();
+
+	std::vector<D3D_FEATURE_LEVEL> featureLevels;
+	featureLevels.emplace_back(D3D_FEATURE_LEVEL_11_1);
+	featureLevels.emplace_back(D3D_FEATURE_LEVEL_11_0);
+	featureLevels.emplace_back(D3D_FEATURE_LEVEL_10_1);
+	featureLevels.emplace_back(D3D_FEATURE_LEVEL_10_0);
+
+	const UINT flags = D3D11_CREATE_DEVICE_DEBUG;
+
+	auto result = D3D11CreateDevice(nullptr, mDriverType, nullptr, flags, featureLevels.data(), 4, D3D11_SDK_VERSION, &mDevice, &mFeatureLevel, &mDeviceContext);
+
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	Microsoft::WRL::ComPtr<IDXGIDevice> dxgiDevice = nullptr;
+	result = mDevice.As(&dxgiDevice);
+
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	result = mDevice.As(&mDebug);
+
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	Microsoft::WRL::ComPtr<IDXGIAdapter> dxgiAdapter = nullptr;
+	result = dxgiDevice->GetAdapter(&dxgiAdapter);
+
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	Microsoft::WRL::ComPtr<IDXGIFactory1> dxgiFactory1 = nullptr;
+	result = dxgiAdapter->GetParent(IID_PPV_ARGS(&dxgiFactory1));
+
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	Microsoft::WRL::ComPtr<IDXGIFactory2> dxgiFactory2 = nullptr;
+	result = dxgiFactory1.As(&dxgiFactory2);
+
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc;
+	ZeroMemory(&swapChainDesc, sizeof swapChainDesc);
+
+	swapChainDesc.Width = window->getWidth();
+	swapChainDesc.Height = window->getHeight();
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 1;
+
+	dxgiFactory2->CreateSwapChainForHwnd(mDevice.Get(), window->getHwnd(), &swapChainDesc, nullptr, nullptr, &mSwapChain1);
+
+	result = mSwapChain1->QueryInterface(IID_PPV_ARGS(&mSwapChain));
+
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	//Create Color Backbuffer
+	Microsoft::WRL::ComPtr<ID3D11Texture2D> backBuffer;
+	result = mSwapChain->GetBuffer(0, IID_PPV_ARGS(&backBuffer));
+
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	result = mDevice->CreateRenderTargetView(backBuffer.Get(), nullptr, mRenderTargetView.ReleaseAndGetAddressOf());
+
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	//Create Depth / Stencil BackBuffer
+	D3D11_TEXTURE2D_DESC depthTextureDesc;
+	depthTextureDesc.Width = window->getWidth();
+	depthTextureDesc.Height = window->getHeight();
+	depthTextureDesc.MipLevels = 1;
+	depthTextureDesc.ArraySize = 1;
+	depthTextureDesc.Format = DXGI_FORMAT_D32_FLOAT;
+	depthTextureDesc.SampleDesc.Count = 1;
+	depthTextureDesc.SampleDesc.Quality = 0;
+	depthTextureDesc.Usage = D3D11_USAGE_DEFAULT;
+	depthTextureDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	depthTextureDesc.CPUAccessFlags = 0;
+	depthTextureDesc.MiscFlags = 0;
+
+	Microsoft::WRL::ComPtr<ID3D11Texture2D> depthStencil = nullptr;
+	result = mDevice->CreateTexture2D(&depthTextureDesc, nullptr, depthStencil.ReleaseAndGetAddressOf());
+
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	D3D11_DEPTH_STENCIL_DESC depthStateDesc;
+	depthStateDesc.DepthEnable = true;
+	depthStateDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+	depthStateDesc.DepthFunc = D3D11_COMPARISON_LESS;
+
+	depthStateDesc.StencilEnable = false;
+	depthStateDesc.StencilReadMask = D3D11_DEFAULT_STENCIL_READ_MASK;
+	depthStateDesc.StencilWriteMask = D3D11_DEFAULT_STENCIL_WRITE_MASK;
+
+	depthStateDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+	depthStateDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_KEEP;
+	depthStateDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	depthStateDesc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+
+	depthStateDesc.BackFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+	depthStateDesc.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_KEEP;
+	depthStateDesc.BackFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	depthStateDesc.BackFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+
+	Microsoft::WRL::ComPtr<ID3D11DepthStencilState> depthState = nullptr;
+
+	result = mDevice->CreateDepthStencilState(&depthStateDesc, depthState.GetAddressOf());
+
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	mDeviceContext->OMSetDepthStencilState(depthState.Get(), 1);
+
+	D3D11_DEPTH_STENCIL_VIEW_DESC depthViewDesc;
+	depthViewDesc.Format = DXGI_FORMAT_D32_FLOAT;
+	depthViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	depthViewDesc.Flags = 0;
+	depthViewDesc.Texture2D.MipSlice = 0;
+
+	result = mDevice->CreateDepthStencilView(depthStencil.Get(), &depthViewDesc, mDepthView.GetAddressOf());
+
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	mDeviceContext->OMSetRenderTargets(1, mRenderTargetView.GetAddressOf(), mDepthView.Get());
+
+	D3D11_VIEWPORT viewport;
+	viewport.Width = static_cast<float>(window->getWidth());
+	viewport.Height = static_cast<float>(window->getHeight());
+	viewport.MinDepth = 0.0f;
+	viewport.MaxDepth = 1.0f;
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+
+	mDeviceContext->RSSetViewports(1, &viewport);
+
+	D3D11_RASTERIZER_DESC rasterizerDesc;
+	rasterizerDesc.CullMode = D3D11_CULL_FRONT;
+	rasterizerDesc.FillMode = D3D11_FILL_SOLID;
+	rasterizerDesc.ScissorEnable = false;
+	rasterizerDesc.DepthBias = 0;
+	rasterizerDesc.DepthBiasClamp = 0.0f;
+	rasterizerDesc.DepthClipEnable = true;
+	rasterizerDesc.MultisampleEnable = false;
+	rasterizerDesc.SlopeScaledDepthBias = 0.0f;
+	rasterizerDesc.AntialiasedLineEnable = true;
+	rasterizerDesc.FrontCounterClockwise = true;
+
+	result = mDevice->CreateRasterizerState(&rasterizerDesc, mRasterizerState.ReleaseAndGetAddressOf());
+
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	mDeviceContext->RSSetState(mRasterizerState.Get());
+
+	return true;
+}
+
+Dx11Render * Dx11Render::instance()
+{
+	if (!mInstance)
+	{
+		mInstance = new Dx11Render();
+
+		if (!mInstance->loadRender())
+		{
+			delete mInstance;
+			mInstance = nullptr;
+		}
+	}
+
+	return mInstance;
+}
+
+Dx11Render::~Dx11Render() = default;
--- GamesArchitecture/Main.cpp
+++ GamesArchitecture/Main.cpp
@@ -1,12 +1,20 @@
 #include "Win32Window.h"
+#include "Dx11Render.h"
+#include <DirectXColors.h>
 
 int WINAPI wWinMain(const HINSTANCE pHInstance, HINSTANCE, LPWSTR, const int pCmdShow)
 {
 	const auto window = Win32Window::instance(pHInstance, pCmdShow);
 	window->run();
+	const auto render = Dx11Render::instance();
 
-	while (window->windowEvents());
+	while (window->windowEvents())
+	{
+		render->clearRenderTargetView(DirectX::Colors::CornflowerBlue);
+		render->present();
+	}
 
+	delete render;
 	delete window;
 
 	return 0;
